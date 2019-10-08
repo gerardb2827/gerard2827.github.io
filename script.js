beeb4f0809b98f9e75f2d28ca4ccb14c8daad0bc
gerardb2827 <56267050+gerardb2827@users.noreply.github.com>
2019-10-08 11:45:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Gerard Bankhead"
+let userName = "gerardb2827";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
